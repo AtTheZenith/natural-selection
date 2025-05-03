e74ab699c21bb0700d390995401a9f40205491d7
zen <atthezenith@proton.me>
2025-05-03 11:14:39 +0300

    Release v0.3.1

--- src/enum.py
+++ src/enum.py
@@ -1,3 +1,20 @@
 import itertools
 
 i = itertools.count()
+n = next
+
+NUL = n(i)
+
+ENTITIES = {
+    "SELF": n(i),
+    "ENEMY": n(i),
+    "FOOD": n(i)
+}
+
+DATA = {
+    "POSITION": n(i),
+    "SIZE": n(i),
+    "SPEED": n(i),
+    "RANGE": n(i),
+    "ENERGY": n(i)
+}
